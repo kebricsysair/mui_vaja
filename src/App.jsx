ee71a51e421351b39b77467cd4623f3140ab3262
kebricsysair <klara.kebric@systemair.si>
2023-02-10 11:30:46 +0100

    Material ui demo za vajo.

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import {
     Grid,
     Toolbar,
     Container,
-    Button
+    Button,
 } from '@mui/material';
 //import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
 import { PhotoCamera } from '@mui/icons-material';
@@ -19,16 +19,18 @@ import useStyles from "./styles";
 /*
 CssBaseline - ponuja vnaprej dolocene stile
 PhotoCamera - ime ikone, ki smo jo importali
+cardMedia se uporablja za predstavizven element - opening and closing elements
  */
 
 const App = () => {
-    const classes = useStyles;
+    const classes = useStyles();
+    const slike = [{index: 1, url: 'nek'}, {index: 2, url: 'nek'}, {index: 3, url: 'nek'}];
     return(
         <>
             <CssBaseline />
             <AppBar position="relative">
                <Toolbar>
-                   <PhotoCamera />
+                   <PhotoCamera className={classes.ikona}/>
                    <Typography variant="h6">
                         Album
                    </Typography>
@@ -46,12 +48,12 @@ const App = () => {
                         <div>
                             <Grid container justify="center" spacing={2}>
                                 <Grid item>
-                                    <Button variant="contained" color="primary">
+                                    <Button className={classes.gumbi} variant="contained" color="primary">
                                         button
                                     </Button>
                                 </Grid>
                                 <Grid item>
-                                    <Button variant="outlined" color="secondary">
+                                    <Button className={classes.gumbi} variant="outlined" color="secondary">
                                         button 2
                                     </Button>
                                 </Grid>
@@ -59,7 +61,42 @@ const App = () => {
                         </div>
                     </Container>
                 </div>
+                <Container className={classes.cardGrid} maxWidth="md">
+                    <Grid container spacing={4}>
+                        {slike.map((slika) => (
+                            <Grid item key={slika.index} xs={12} sm={6} md={4}>
+                                <Card className={classes.card}>
+                                    <CardMedia
+                                        className={classes.cardMedia}
+                                        image="https://source.unsplash.com/random"
+                                        title="Image title"
+                                    />
+                                    <CardContent className={classes.cardContent}>
+                                        <Typography gutterBottom variant="h5">
+                                            Heading
+                                        </Typography>
+                                        <Typography>
+                                            Opis slike
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Button size="small" color="primary">View</Button>
+                                        <Button size="small" color="primary">Edit</Button>
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Container>
             </main>
+            <footer className={classes.footer}>
+                <Typography gutterBottom variant="h5" align="center">
+                    Footer
+                </Typography>
+                <Typography gutterBottom variant="subtitle1" align="center" color="textSecondary">
+                    Opis
+                </Typography>
+            </footer>
         </>
 );
 }
--- src/styles.js
+++ src/styles.js
@@ -1,5 +1,7 @@
 import { makeStyles } from "@mui/styles";
+import {paperClasses} from "@mui/material";
 
+/*
 const useStyles = makeStyles((theme) => {
     return{
         izgled: {
@@ -8,4 +10,43 @@ const useStyles = makeStyles((theme) => {
         }
     }
 });
+
+ */
+
+const useStyles = makeStyles({
+    izgled: {
+        background: "whitesmoke",
+    },
+    drugizgled: {
+        background: "pink",
+    },
+    ikona: {
+        marginRight: '20px'
+    },
+    gumbi: {
+        marginLeft: '30px',
+        marginTop: '20px',
+        color: 'black'
+    },
+    cardGrid: {
+        padding: '20px 0'
+    },
+    card: {
+        minWidth: '280px',
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    cardMedia: {
+        paddingTop: '56.25%'
+    },
+    cardContent: {
+        flexGrow: 1
+    },
+    footer:{
+        backgroundColor: "whitesmoke",
+        padding: '50px 0px',
+        color: 'black'
+    }
+})
 export default useStyles;
